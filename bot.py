71e583468644385a283bd5b02015ab9240f64579
Zac Joffe <zacharyjoffe@gmail.com>
2020-06-05 16:39:23 -0400

    Help command

--- bot.py
+++ bot.py
@@ -33,12 +33,12 @@ maps = default_maps.copy()
 
 client = commands.Bot(command_prefix='!')
 
-@client.command(name='map')
+@client.command(name='map', help='Randomly selects a map from the pool')
 async def map(ctx):
     print(maps)
     await ctx.send(maps[random.randint(0, len(maps) - 1)])
 
-@client.command(name='add')
+@client.command(name='add', help='Adds a given map to the pool')
 async def add(ctx, *args):
     new_map = ' '.join(args)
 
@@ -49,13 +49,13 @@ async def add(ctx, *args):
         maps.append(new_map)
         await ctx.send("Added {} to map pool".format(new_map))
 
-@client.command(name='reset')
+@client.command(name='reset', help='Resets the pool to the original list of Black Ops 3 maps')
 async def reset(ctx):
     maps[:] = default_maps.copy()
     print(maps)
     await ctx.send("Reset maps to original list")
 
-@client.command(name='pop')
+@client.command(name='pop', help='Remove most recently inserted map from pool')
 async def reset(ctx):
     # TODO - do not pop original items
     last_map = maps[-1]
@@ -63,7 +63,7 @@ async def reset(ctx):
     print(maps)
     await ctx.send("Removed {} from list".format(last_map))
 
-@client.command(name='remove')
+@client.command(name='remove', help='Remove given map from pool')
 async def reset(ctx, *args):
     map_name = ' '.join(args)
 
@@ -74,7 +74,7 @@ async def reset(ctx, *args):
         maps.remove(map_name)
         await ctx.send("Removed {} from list".format(map_name))
 
-@client.command(name='list')
+@client.command(name='list', help='List the current maps in the pool')
 async def list_maps(ctx):
     map_list = "List of the current map pool:"
     for i, v in enumerate(maps, 1):
