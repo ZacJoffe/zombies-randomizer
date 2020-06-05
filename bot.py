c781f45ae44e2fafb9488bfd7df37d903d655212
Zac Joffe <zacharyjoffe@gmail.com>
2020-06-05 16:27:49 -0400

    Command to list all maps in pool

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
 import os
-# import discord
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
 
-load_dotenv()
+# load .env file if exists
+# DISCORD_TOKEN env var is set as a config var within Heroku
+load_dotenv() 
 TOKEN = os.getenv('DISCORD_TOKEN')
-# print(TOKEN)
 
 random.seed()
-# client = discord.Client()
 
 default_maps = [
     "Shadows of Evil",
@@ -33,7 +32,6 @@ default_maps = [
 maps = default_maps.copy()
 
 client = commands.Bot(command_prefix='!')
-# bot = commands.Bot(command_prefix='!')
 
 @client.command(name='map')
 async def map(ctx):
@@ -76,28 +74,18 @@ async def reset(ctx, *args):
         maps.remove(map_name)
         await ctx.send("Removed {} from list".format(map_name))
 
+@client.command(name='list')
+async def list_maps(ctx):
+    map_list = "List of the current map pool:"
+    for i, v in enumerate(maps, 1):
+        map_list += '\n{}. {}'.format(i, v)
+
+    print(map_list)
+    await ctx.send(map_list)
 
 # @client.event
 # async def on_ready():
 #     print(f'{client.user} has connected to Discord!')
-
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('!map'):
-#         await message.channel.send(maps[random.randint(0, len(maps) - 1)])
-
-#     if message.content.startswith('!moss'):
-#         await message.channel.send("Nacht der Untoten")
-
-#     if message.content.startswith('!add'):
-#         await message.channel.send(maps[random.randint(0, len(maps) - 1)])
-
-#     if message.content.startswith('!reset'):
-#         default_maps = maps
 
 
 client.run(TOKEN)
